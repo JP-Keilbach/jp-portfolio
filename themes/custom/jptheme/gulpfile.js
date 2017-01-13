5b978058b411ba22941bf7fbe41862f90eb91ed2
jpkeilbach <jan-pieter.keilbach@gmail.com>
2017-01-13 12:39:29 +0100

    Adding jptheme

---
+++ themes/custom/jptheme/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'bower_components/normalize.scss/sass',
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+var browserSync = require('browser-sync').create();
+
+// Setting up the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        proxy: "http://drupal-sandbox.dev"
+    })
+});
+
+// Setting up the sass task
+gulp.task('sass', function() {
+  // Taking the path from the above objekt
+  return gulp.src('scss/**/*.scss')
+    //
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css')) // Output to destination folder
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+
+// This is the default task - which is run when 'gulp' is run
+// The tasks passed in as an array are run before the tasks within the funciton
+gulp.task('default', ['browserSync', 'sass'], function() {
+  // Watch the files in the path object, and when there is a change, run the functions in the array
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  // gulp.watch(['scss/**/*.scss'], browserSync.reload);
+});
