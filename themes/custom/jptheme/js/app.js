77357bea5682ad4796acea8aa794a2af5de3c99a
jpkeilbach <jan-pieter.keilbach@gmail.com>
2017-01-24 18:46:19 +0100

    work in progress with slick slider

--- themes/custom/jptheme/js/app.js
+++ themes/custom/jptheme/js/app.js
@@ -1,5 +1,20 @@
 
 (function($) {
     $(document).foundation();
+
+    $('.slick').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.slicker').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
 }(jQuery));
 
