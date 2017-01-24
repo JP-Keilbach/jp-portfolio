eac22f8ed00478ffa4d10d3d5920565d1c452fc9
jpkeilbach <jan-pieter.keilbach@gmail.com>
2017-01-24 22:40:09 +0100

    messing around with slick

--- themes/custom/jptheme/js/app.js
+++ themes/custom/jptheme/js/app.js
@@ -9,12 +9,35 @@
         autoplaySpeed: 2000
     });
 
-    $('.slicker').slick({
+    $('.ffslick-hero').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+    });
+
+    $('.slick-portfolio').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+
+    $('.slick-hero').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
 
 }(jQuery));
 
