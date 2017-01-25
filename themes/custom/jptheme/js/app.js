557898fd9a18032fe6026263aeeb0ba0ab636b4a
jpkeilbach <jan-pieter.keilbach@gmail.com>
2017-01-25 09:50:32 +0100

    working on portfolio section

--- themes/custom/jptheme/js/app.js
+++ themes/custom/jptheme/js/app.js
@@ -9,23 +9,6 @@
         autoplaySpeed: 2000
     });
 
-    $('.ffslick-hero').slick({
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        infinite: true,
-    });
-
-    $('.slick-portfolio').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
-
-
     $('.slick-hero').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -36,6 +19,36 @@
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+    $('.slick-portfolio').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
 
 
 
