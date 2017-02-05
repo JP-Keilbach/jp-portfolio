de48af9782ab1c0b9cd0ac72a674a71d23019f1a
jpkeilbach <jan-pieter.keilbach@gmail.com>
2017-02-06 00:32:23 +0100

    working on forms, contact and buttons

--- themes/custom/jptheme/js/app.js
+++ themes/custom/jptheme/js/app.js
@@ -443,5 +443,102 @@
 
     }
 
+    /**
+     * --------------------------------
+     * Load Snap animated Submit Button
+     * --------------------------------
+     */
+
+    /** Change submit button (input) in contact form **/
+    var submitinput = document.getElementById("edit-submit");
+    var submitbtn = document.getElementById("button-submit");
+    submitinput.parentNode.replaceChild(submitbtn, submitinput);
+
+    var sSubmit = Snap("#submit"), btnSubmit; // This will use an existing svg element (not a div)
+
+    Snap.load('themes/custom/jptheme/assets/images/button-submit.svg', function (response) {
+        btnSubmit = response;
+        sSubmit.append(btnSubmit);
+        initialiseSubmit();
+    });
+
+    initialiseSubmit = function() {
+
+        /**
+         * Put segments in place & group them
+         */
+        var segmentSub2 = sSubmit.select('#segment-2');
+        var segmentSub5 = sSubmit.select('#segment-5');
+        var segmentGroup = sSubmit.group(segmentSub2, segmentSub5);
+        var sendSignal = sSubmit.select('#typo');
+
+        console.log('output segmentGroup var: ' + segmentGroup);
+        // var twitterSymbol = sTwitter.select('#twitter-symbol');
+
+        /** Fill everything in **/
+        sSubmit.attr({fill: '#4d4d4d'});
+
+        /** Set animating to true **/
+        var animating = true;
+
+        /** Begin animation and loop **/
+        function animOnButton() {
+
+            if (animating) {
+
+                setTimeout(function () {
+                    sendSignal.animate({
+                        fill:'#2e2e2e'
+                    }, 200);
+                }, 0);
+
+                setTimeout(function () {
+                    segmentSub2.stop().animate({
+                        transform: 't-5,0 r0 133,222 33,32)',
+                        opacity: '0'
+                    }, 500, function () { segmentSub2.animate({
+                        transform: 't0,0 r0 133,222 33,32)',
+                        opacity: '1'
+                    }, 0, animOnButton)});
+
+                    segmentSub5.stop().animate({
+                        transform: 't5,0 r0 133,222 33,32)',
+                        opacity: '0'
+                    }, 500, function () { segmentSub5.animate({
+                        transform: 't0,0 r0 133,222 33,32)',
+                        opacity: '1'
+                    }, 0, animOnButton)});
+                }, 0);
+            }
+        }
+
+        /** Kicks in when mouse leaves element **/
+        function animOutButton() {
+
+            setTimeout(function () {
+                sendSignal.animate({
+                    fill:'#4d4d4d'
+                }, 200);
+            }, 0);
+        }
+
+        /**
+         * Fills segment with certain color
+         * @param segement: Snap object
+         * @param color: Hex Sting
+         * @param timer: Duration in ms
+         * @param callback: function to call after completion
+         */
+        /* var resetColor = function (segment, color, timer, mina, callback) {
+         if (segment && color && timer) {
+         segment.animate({ fill: color }, timer, mina, callback);
+         }
+         }; */
+
+        /** Trigger animation on hover. Set animating to false by mouseleave. Animation stops. **/
+        $('#button-submit').hover(function () {animating = true; animOnButton()}, function () {animating = false; animOutButton()});
+
+    }
+
 }(jQuery));
 
